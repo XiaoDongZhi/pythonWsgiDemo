fa8b0761b5fdc62e2a81b31b0fcb44451ea411a3
Michael Rice <me@michaelrice.com>
2014-06-05 21:42:36 -0700

    getting sophisticated

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
-# no matter what the environ['PATH_INFO'], just say hello
 def application(environ, start_response):
     ctype = "text/html"
-    response_body = "Hello, world"
+    response_body = ""
+    if environ["PATH_INFO"] == "/" or environ["PATH_INFO"] == "/hello":
+        response_body = "Hello, world"
+    elif environ["PATH_INFO"] == "/goodbye":
+        response_body = "Ok see ya"
     status = "200 OK"
     response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
     start_response(status, response_headers)
