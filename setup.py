84547a9db1f5f6ce3438d516e1ea11ab6b1f6725
Michael Rice <me@michaelrice.com>
2014-06-09 12:58:28 -0700

    oops, renamed wrong file

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+
+setup(name='PythonDemo',
+      version='1.0',
+      description='RHC-West Demo Python App',
+      author='Michael Rice',
+      author_email='mrice@redhat.com',
+      url='http://www.python.org/sigs/distutils-sig/')
